6cb19a3f49f00282ef1498685f7b4a7576aeb17d
Lucía España <99997724+luespana@users.noreply.github.com>
2022-08-02 19:12:16 -0300

    backend

---
+++ backend/models/novedadesModel.js
@@ -0,0 +1,22 @@
+var pool = require("./bd");
+
+async function getNovedades() {
+  var query = 'select * from novedades';
+  var rows = await pool.query(query);
+  return rows
+}
+
+async function postNovedades(obj) {
+  try {
+    var query = "insert into novedades set ?"
+    var rows = await pool.query(query, [obj])
+    return rows
+
+  } catch (error) {
+    console.log(error)
+    throw error;
+    
+  }
+}
+
+module.exports = { getNovedades, postNovedades}
--- backend/routes/admin/novedades.js
+++ backend/routes/admin/novedades.js
@@ -1,13 +1,46 @@
 var express = require("express");
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel')
 
 /* GET login page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades()
+
   res.render("admin/novedades", {
     layout: "admin/layout",
-    persona: req.session.nombre
+    persona: req.session.nombre,novedades
   });
 });
+
+router.get('/agregar', (req,res,next) => {
+  res.render("admin/agregar", {
+    layout: "admin/layout"
+  });
+})
+
+router.post('/agregar', async (req,res,next) => {
+  const data = req.body
+  try {
+    if (data.titulo != "" && data.subtitulo != "" && data.cuerpo !=""){
+      await novedadesModel.postNovedades(data);
+      res.redirect('/admin/novedades')
+    }else{
+      res.render("admin/agregar", {
+        layout: "admin/layout",
+        error: true,
+        message: "Todos los campos son requeridos"
+      });
+    }
+  } catch (error) {
+    console.error(error)
+    res.render("admin/agregar", {
+      layout: "admin/layout",
+      error: true,
+      message: "No se cargo la novedad"
+    });
+  }
+})
 
 
 module.exports = router;
